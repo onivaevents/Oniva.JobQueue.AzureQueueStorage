33a2d701bd1732bf3852bf853aacacf4178a34aa
Simon Gadient <simon.gadient@oniva.events>
2025-06-03 22:49:23 +0200

    Fix functional test class loading

--- Tests/Functional/Queue/AzureStorageQueueTest.php
+++ Tests/Functional/Queue/AzureStorageQueueTest.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace Oniva\JobQueue\AzureQueueStorage\Tests\Functional\Queue;
 
 use Flowpack\JobQueue\Common\Exception;
-use Flowpack\JobQueue\Common\Queue\Message;
 use Flowpack\JobQueue\Common\Tests\Functional\AbstractQueueTest;
 use Oniva\JobQueue\AzureQueueStorage\Queue\AzureQueueStorage;
 
-class AzureQueueStorageTest extends AbstractQueueTest
+class AzureStorageQueueTest extends AbstractQueueTest
 {
     /**
      * @throws Exception
